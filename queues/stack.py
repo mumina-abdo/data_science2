84042788c2f8ac2266ec0bbdb9e6cd410a4813d0
mumina-abdo <muminaboruabdo@gmail.com>
2024-04-25 11:14:08 +0300

    add stack operations

---
+++ queues/stack.py
@@ -0,0 +1,16 @@
+#Create a stack and then add items into it
+names =[]
+names.append('sadho')
+names.append('Halima')
+names.append('Hawo')
+names.append('Latu')
+print(names)
+
+#Accessing an item in a stacks
+names=['sadho','Halima','Hawo','Latu']
+print(names[1])
+
+#Remove an item in a stack
+names=['sadho','Halima','Hawo','Latu']
+names.pop()
+print(names)
